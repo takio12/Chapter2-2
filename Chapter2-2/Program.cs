4272bf12404c37d3ce5a01ed408ff7cc42c5835a
TAKIO-PC\takio <taka21centry@gmail.com>
2017-12-08 01:12:26 +0900

    解けた

--- Chapter2-2/Program.cs
+++ Chapter2-2/Program.cs
@@ -10,6 +10,27 @@ namespace Chapter2_2
     {
         static void Main(string[] args)
         {
+            if (args.Length >= 1 && args[0] == "-tom")
+                PrintinchToMeterList(1, 10);
+            else
+                PrintMeterToInchList(1, 10);
+        }
+
+        static void PrintinchToMeterList(int start,int stop)
+        {
+            for(int inch = start; inch <= stop; inch++)
+            {
+                double meter = inchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch ={1:0.0000}m", inch, meter);
+            }
+        }
+        static void PrintMeterToInchList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double inch = inchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m ={1:0.0000}inch", meter, inch);
+            }
         }
     }
 }
---
+++ Chapter2-2/inchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2_2
+{
+    public class inchConverter
+    {
+        private const double ratio = 0.0254;
+
+        public static double FromMeter(double meter)
+        {
+            return meter / ratio;
+        }
+        public static double ToMeter(double inch)
+        {
+            return inch * ratio;
+        }
+    }
+}
